93a889ce445ab9946192d0632fc4091544aa67c5
rafiul hasan <rafiul.hasan@technovicinity.com>
2023-09-13 15:44:01 +0600

    Initial Commit

---
+++ FlyerBuilder/PDFPreviewViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+import PDFKit
+
+class PDFPreviewViewController: UIViewController {
+  
+  public var documentData: Data?
+  
+  @IBOutlet weak var pdfView: PDFView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let data = documentData {
+      pdfView.document = PDFDocument(data: data)
+      pdfView.autoScales = true
+    }
+  }
+}
